312ce2b9908de5683f7212ebd13a322f4b474410
jman <flute999@gmail.com>
2025-12-13 03:25:59 +0900

    Upload initial blank page

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+declare global {
+  /*  example
+  interface Window {
+    elecPlanit?: {
+      bucket: any;
+      todo: any;
+    };
+  }
+
+  const _USE_ELECTRON_: boolean;
+  const _WEB_BASE_PATH_: string;*/
+}
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
